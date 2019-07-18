d847f6e1483dc9aa8dd17a9efd111bc919b5eca2
Josh <Kersting.Josh@Gmail.com>
2019-07-18 18:07:25 -0400

    solved RPS, added cache

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,38 @@
 
 import sys
 
+import time
+start = time.time()
+
+
 def rock_paper_scissors(n):
-  pass 
+    moves = ["rock", "paper", "scissors"]
+    cache = {
+        0: [[]],
+        1: [["rock"], ["paper"], ["scissors"]]
+    }
+
+    def recurse(n):
+        nonlocal moves, cache
+
+        # cache starts with base case and edge case
+        if n in cache:
+            return cache[n]
+
+        # this list comprehension looks so smoooooth
+        cache[n] = [r + [m] for r in recurse(n-1) for m in moves]
+
+        return cache[n]
+
+    return recurse(n)
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(len(rock_paper_scissors(num_plays)))
+    else:
+        print('Usage: rps.py [num_plays]')
+
+    end = time.time()
+    print(end - start)
